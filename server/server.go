8b58200a3fcef65c8fb7b23da890f3f6d2060de2
ishizakamasaki <robomasaki2@gmail.com>
2021-03-19 08:33:37 +0900

    feat: cors
    Reactからアクセスができるように、corsの実装をした。

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/MasakiHinata/go/graphql/graph"
 	"github.com/MasakiHinata/go/graphql/graph/generated"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
@@ -15,7 +16,13 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	})
+
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%d/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
